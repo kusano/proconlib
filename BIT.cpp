10539d7a8103fd2f5731550d24eaaab281dab7b9
kusano <kusano@users.noreply.github.com>
2017-01-14 16:38:12 +0900

    Update BIT.cpp

--- BIT.cpp
+++ BIT.cpp
@@ -18,7 +18,7 @@ public:
         for (; i<n; i|=i+1)
             v[i] += x;
     }
-    //  return a[0]+a[1]+�c+a[i-1]
+    //  return a[0]+a[1]+…+a[i-1]
     int sum(int i) {
         int s = 0;
         for (i--; i>=0; i=(i&(i+1))-1)
